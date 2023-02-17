c0032333de6cc273bb4a13ebaecc1fba3cc5fd7b
vashmeen <_@null.err>
2023-02-17 14:12:57 -0800

    fix: types

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
         <h1 className="">
           <div className="flex gap-1 items-baseline">
             <span className="H1 fw-900">Hello! </span>
-            <img src="/3d-icons/waving-hand.png" alt="waving-hand" className=" w-15" aria-hidden="" />
+            <img src="/3d-icons/waving-hand.png" alt="waving-hand" className=" w-15" aria-hidden="true" />
           </div>
           <span className="block mt-10 max-w-100">I am Hamid</span>
         </h1>
